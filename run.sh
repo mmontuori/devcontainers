d14a35108f8525922a0888c7a965032b6539fe8a
mmontuori <michael.montuori@gmail.com>
2025-01-16 23:27:36 -0500

    added jupyter notebook

--- run.sh
+++ run.sh
@@ -10,9 +10,15 @@ if [ "$1" == "" ]; then
 fi
 
 if [ "$container_user" == "" ]; then
-    echo "congainer_user env variable not found, default to ${container_user}"
     container_user="devuser"
 fi
+
+if [ "$container_port" != "" ]; then
+    container_args="-p${container_port}:${container_port}"
+fi
+
+echo "container_user=${container_user}"
+echo "container_port=${container_port}"
 
 if [ "$2" != "" ];then
     cmd="${2} ${3} ${4} ${5} ${6}"
@@ -21,4 +27,4 @@ else
 fi
 echo "running ${cmd} as starting command..."
 
-docker run --rm --user ${container_user}:devgroup -w /home/${USER} -v ${HOME}:/home/${USER} --rm -ti ${label}/$1 ${cmd}
+docker run --rm --user ${container_user}:devgroup $container_args -w /home/${USER} -v ${HOME}:/home/${USER} --rm -ti ${label}/$1 ${cmd}
