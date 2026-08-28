a3c99b2414a26b9e8e9c0e3d55f68c292daba353
mmontuori <michael.montuori@gmail.com>
2026-08-28 17:24:23 +0000

    changes to the initial CMD

--- run.sh
+++ run.sh
@@ -34,6 +34,12 @@ fi
 
 echo "running ${cmd} as starting command..."
 
+if [ "$run_detached" == "true" ]; then
+    run_args="--detach"
+else
+    run_args=""
+fi
+
 cd ${running_dir}
 
-$container_runtime run --name $container_name --hostname $container_name --security-opt label=disable --rm --user ${user_string} ${gpu_args} $container_args -w /home/${container_user} -v ${dev_volume}:/home/${container_user} -ti ${label}/$1 ${cmd}
+$container_runtime run ${run_args} --name $container_name --hostname $container_name --security-opt label=disable --rm --user ${user_string} ${gpu_args} $container_args -w /home/${container_user} -v ${dev_volume}:/home/${container_user} -ti ${label}/$1 ${cmd}
---
+++ stop.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source "$(dirname "$0")/setenv.sh"
+
+echo "using container runtime: $container_runtime"
+
+if [ "$1" == "" ]; then
+    echo "choose one of the following container files to exec:"
+    echo ""
+    find "${running_dir}/containerfiles/" -maxdepth 1 -type f -printf "%f\n"
+    exit
+fi
+
+if [ "$2" != "" ];then
+    cmd="${2} ${3} ${4} ${5} ${6}"
+else
+    cmd="su - ${container_user}"
+fi
+echo "stopping container ${1}..."
+
+docker_id=`$container_runtime ps | grep "$1" | awk '{ print $1 }'`
+
+cd ${running_dir}
+
+$container_runtime stop ${docker_id}
